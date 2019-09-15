fc2c0fed82c01fab005e60a7f9c4091ff7ac278a
= <=>
2019-09-16 01:48:02 +0530

    16-09-2019

--- src/department/Cardiology.js
+++ src/department/Cardiology.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import  axios from 'axios'
 
 export default class Cardiology extends React.Component{
 
   constructor( props ){
     super( props )
     this.state = {
-      gitList: ""
+      isLoaded: false,
+      doctorList: ""
     }
   }
   componentDidMount(){
@@ -14,30 +14,28 @@ export default class Cardiology extends React.Component{
   }
 
    getData = () =>{
-    axios.get('https://pgibotapi.herokuapp.com/getAllDepartments')
-    .then(response =>{
-      console.log(response.data);
+
+    fetch('https://pgibotapi.herokuapp.com/getDepartmentDoctors?departmentName=Cardiology')
+    .then(result => result.json())
+    .then(mainDoctorList =>{
+      this.setState({
+        isLoaded: true,
+        doctorList: mainDoctorList
+      })
     })
-    .catch(error =>{
-      console.log(error);
-    });
-    // axios({
-    //   method: "GET",
-    //   url: "https://pgibotapi.herokuapp.com/getDepartmentDoctors",
-    //    headers: {
-    //         "Content-Type": "application/json"
-    //    },
-    //    data: {
-    //     departmentName : "Cardiology"
-    //    }
-    // }).then( response =>{
-    //   console.log( response.data)
-    // })
   }
     render(){
+      var { isLoaded, doctorList } = this.state
+      if( !isLoaded )
+      {
+        return(
+          <h1>Data is Loading....</h1>
+        )
+      }
+
     return(
-    <div className="container mt-5">
-        <h5 className="display-5 mb-3">You are looking for Cardiology..</h5>
+<div className="container mt-5">
+  <h5 className="display-5 mb-3">You are looking for Cardiology..</h5>
 <table className="table table-striped">
   <thead className="bg-dark text-light">
     <tr>
@@ -47,15 +45,13 @@ export default class Cardiology extends React.Component{
       <th scope="col">Handle</th>
     </tr>
   </thead>
+
   <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
+    {
+      // console.log(doctorList.length)
+    }
   </tbody>
 </table>
-        </div>
-    )
+</div>
+  )
 }}
